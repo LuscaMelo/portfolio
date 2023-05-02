3cbe7258315c4593690105b540ff9930d40e71ad
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-05-02 09:58:43 -0300

    Project card com dados dinâmicos

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,18 +1,34 @@
 import { BsGithub } from 'react-icons/bs'
 import { VscVmRunning } from 'react-icons/vsc'
 
-export const ProjectCard = () => {
+interface Props {
+    img: string
+    name: string
+    description: string
+    linkTry: string
+    linkGit: string
+}
+
+export const ProjectCard = (props: Props) => {
     return (
         <div>
-            <div>
-                <img className="object-cover" src="shopping-cart.png" alt="" />
+            <div className='w-full object-cover'>
+                <img className="h-full w-full object-cover object-top" src={props.img} alt="" />
             </div>
-            <div className="p-4">
-                <h3 className="text-lg mb-3">Shopping Cart</h3>
-                <p className="text-sm text-gray-500">Page of a fictitious product, being able to add it to the shopping cart.</p>
-                <div className="flex gap-5 mt-5 h-[40px]">
-                    <button className="flex items-center justify-center gap-3 w-[50%] bg-primary"><VscVmRunning />Try it</button>
-                    <button className="flex items-center justify-center gap-3 w-[50%] bg-primary"><BsGithub />Code</button>
+            <div className="p-4 flex flex-col justify-between min-h-[180px] h-full">
+                <h3 className="text-lg mb-3">{props.name}</h3>
+                <p className="text-sm text-gray-500">{props.description}</p>
+                <div className="flex gap-5 mt-5">
+                    <button className="w-[50%] bg-primary">
+                        <a className='flex items-center justify-center gap-3 p-2' href={props.linkTry} target="_blank" rel="noopener noreferrer">
+                            <VscVmRunning />Try it
+                        </a>
+                    </button>
+                    <button className="flex items-center justify-center gap-3 w-[50%] bg-primary">
+                        <a className='flex items-center justify-center gap-3 p-2' href={props.linkGit} target="_blank" rel="noopener noreferrer">
+                            <BsGithub />Code
+                        </a>
+                    </button>
                 </div>
             </div>
         </div >
--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -14,17 +14,37 @@ export const Projects = () => {
                         <span className="absolute top-[0px] left-[60px] text-xs mt-1 text-cyan-700"> - Benjamin Franklin</span>
                     </div>
                     <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4 mt-10">
-                        <div className="bg-glass w-[1fr]">
-                            <ProjectCard />
+                        <div className="bg-secondary w-[1fr]">
+                            <ProjectCard
+                                img="shopping-cart.png"
+                                name="Shopping Cart"
+                                description="Page of a fictitious product, being able to add it to the shopping cart."
+                                linkTry="https://shopping-cart-luscamelo.vercel.app/"
+                                linkGit="https://github.com/LuscaMelo/shopping-cart" />
                         </div>
-                        <div className="bg-glass w-[1fr]">
-                            <ProjectCard />
+                        <div className="bg-secondary w-[1fr]">
+                            <ProjectCard
+                                img="pokedex.png"
+                                name="Pokedex"
+                                description="Listing of pokemons, where the data is consumed in an API (PokeAPI)."
+                                linkTry="https://lucasmelo-pokedex-dio.surge.sh/"
+                                linkGit="https://github.com/LuscaMelo/js-developer-pokedex" />
                         </div>
-                        <div className="bg-glass w-[1fr]">
-                            <ProjectCard />
+                        <div className="bg-secondary w-[1fr]">
+                            <ProjectCard
+                                img="music-production.png"
+                                name="Music Production Website"
+                                description="Simple website about a music production course."
+                                linkTry="https://music-production-gamma.vercel.app/"
+                                linkGit="https://github.com/LuscaMelo/music-production" />
                         </div>
-                        <div className="bg-glass w-[1fr]">
-                            <ProjectCard />
+                        <div className="bg-secondary w-[1fr]">
+                            <ProjectCard
+                                img="gym.png"
+                                name="Gym Website"
+                                description="Simple website about a gym."
+                                linkTry="https://gym-luscamelo.vercel.app/"
+                                linkGit="https://github.com/LuscaMelo/gym" />
                         </div>
                         <div className="flex justify-center items-center p-5 bg-glass w-[1fr]">
                             <p className='text-white text-xl flex gap-1 ml-1 items-baseline'>...<p className='text-xs'> more soon</p></p>
