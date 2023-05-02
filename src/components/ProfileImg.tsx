02b84f68edd8e4fb5c819e9705937bda01cf6b4a
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-05-02 00:27:22 -0300

    Animação na página Home

--- src/components/ProfileImg.tsx
+++ src/components/ProfileImg.tsx
@@ -36,11 +36,18 @@ export const ProfileImg = () => {
             animate="show"
             whileHover="focus"
         >
-            <img src="lucas.jpg" className='rounded-full w-[220px] md:w-[350px] lg:w-[500px] mb-3' alt=""></img>
-            <div className='w-[25%] h-[25%] bg-cyan-900 absolute top-[0%] right-[2%] rounded-full'>
+            <m.div
+                className="absolute top-0 w-full h-full border-2 border-cyan-800 z-10" style={{ borderRadius: "67% 33% 19% 81% / 62% 47% 53% 38%" }}
+                animate={{ rotate: 1460, transition: { duration: "100" } }}
+            ></m.div>
+            <m.div
+                className="absolute top-0 w-full h-full border-2 border-gray-600 z-10" style={{ borderRadius: "24% 76% 62% 38% / 74% 19% 81% 26%" }}
+                animate={{ rotate: 1360, transition: { duration: "120" } }}
+            ></m.div>
+            <img src="lucas.jpg" style={{ borderRadius: "30% 70% 70% 30% / 30% 30% 70% 70% " }} className='relative w-[280px] md:w-[350px] lg:w-[500px] mb-4 z-20' alt=""></img>
+            <div className='w-[25%] h-[25%] bg-cyan-900 absolute top-[0%] right-[2%] rounded-full z-20'>
                 <img src="coding.svg" className='lg:p-5 p-2' alt="" />
             </div>
-            <div className=' flex items-center justify-center w-[12%] h-[12%] bg-gray-500 opacity-70 absolute top-[21%] right-[0%] rounded-full'></div>
         </m.div>
     )
 }
